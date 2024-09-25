6b5f5f8b79737480f8e4b66306efab1ad41e75fa
user <user@HS-NOTEBOOK>
2024-09-25 19:33:54 +0900

    New : updateProduct GET 까지 완성
    09.25.수
    POST 방식 테스트 하는 법 강구하기

--- src/client/app/RestHttpTestApp.java
+++ src/client/app/RestHttpTestApp.java
@@ -14,6 +14,8 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.json.simple.JSONObject;
@@ -34,37 +36,91 @@ public class RestHttpTestApp {
 	public static void main(String[] args) throws Exception {
 		
 		// GET/Post, uri, param
-		RestHttpTestApp.useCodehaus("GET", "/product/json/getProduct/10006/김박사/manage", null);
+		
+		// listProduct
+//		RestHttpTestApp.useCodehaus("GET", "/json/product/listProduct", "menu=search");
+		
+		// getProduct
+//		RestHttpTestApp.useCodehaus("GET", "/json/product/getProduct/10036", null);
+		
+		// updateProduct Get
+//		RestHttpTestApp.useCodehaus("Get", "/json/product/updateProduct/10036", null);
+		
+		// updateProduct Post
+		RestHttpTestApp.useCodehaus("Post", "/json/product/updateProduct", 
+									"prodNo=10036&"
+								  + "prodName=testRestChanged&"
+								  + "manuDate=2024-09-25&"
+								  + "price=909090&"
+								  + "fileName=empty.GIF");
 
 	}
 	
 	
-	public static void useCodehaus(String getOrPost, String uri, String param) 
+	public static void useCodehaus(String method, String uri, String param) 
 								   throws Exception {
 		
-		System.out.println(String.format("getOrPost= %s, uri= %s, param= %s\n", getOrPost, uri, param));
+		System.out.println(String.format("method= %s, uri= %s, param= %s\n", method, uri, param));
 		
+		/* Request 로직 */
 		param = (param == null)? "" : param;
-		getOrPost = (getOrPost == null)? "GET" : getOrPost.toUpperCase();
+		method = (method == null)? "GET" : method.toUpperCase();
 		
 		HttpClient httpClient = new DefaultHttpClient();
 		
-		String url = "http://127.0.0.1:8080"+ uri + ((param != null && !param.equals(""))? "?" + param : "");
+		String url = "http://127.0.0.1:8080"+ uri + ((method.equals("GET"))?((param != null && !param.equals(""))? "?" + param : ""):"");
 		
-		HttpRequestBase httpGetOrPost = null;
+		System.out.println("url= "+url);
+		System.out.println();
 		
-		if (getOrPost.equals("GET")) {
-			httpGetOrPost = new HttpGet(url);
+		HttpResponse httpResponse = null;
+		
+		if (method.equals("GET")) {
+			HttpGet httpGet = new HttpGet(url);
 			
-		} else if (getOrPost.equals("POST")) {
-			httpGetOrPost = new HttpPost(url);
+			httpGet.setHeader("Accept", "application/json");
+			httpGet.setHeader("Content-Type", "application/json");
+			
+			httpResponse = httpClient.execute(httpGet);
+			
+		} else if (method.equals("POST")) {
+			HttpPost httpPost = new HttpPost(url);
+			
+			httpPost.setHeader("Accept", "application/json");
+			httpPost.setHeader("Content-Type", "application/json");
+			
+			HttpEntity httpPostEntity = null;
+			
+			// POST 인 경우 JSON 객체로 데이터를 넘겨줘야한다
+			/* 데이터 넘기는 로직 추가 - param으로 받아서 파싱해서 JSON 객체에 넣어주기 */
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+			
+			if (!(param == null || param.equals(""))) {
+				
+				String[] params = param.split("&");
+				
+				for (String p : params) {
+					String key = p.split("=")[0];
+					String value = p.split("=")[1];
+					
+					builder.addTextBody(key, value);
+				}
+				
+			} else {
+				throw new IllegalArgumentException("Error : 파라미터 값을 잘못 입력");
+				
+			}
+			
+			System.out.println("builder= "+builder);
+			httpPostEntity = builder.build();
+			
+			httpPost.setEntity(httpPostEntity);
+			httpResponse = httpClient.execute(httpPost);
 			
 		}
 		
-		httpGetOrPost.setHeader("Accpet", "application/json");
-		httpGetOrPost.setHeader("Content-Type", "application/json");
 		
-		HttpResponse httpResponse = httpClient.execute(httpGetOrPost);
+		/* Response 이후 로직 */
 		
 		System.out.println(httpResponse);
 		System.out.println();
@@ -75,34 +131,67 @@ public class RestHttpTestApp {
 		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 		
 		JSONObject jsonObj = (JSONObject) JSONValue.parse(br);
+		System.out.println("\n전달받은 jsonObject.toString()=\n");
 		System.out.println(jsonObj);
 		
 		ObjectMapper objectMapper = new ObjectMapper();
 		
 		System.out.println("\n\n=============================================");
 		
-		if (uri.contains("product")) {
-			
-			if (uri.contains("list")) {
-				List<JSONObject> jsonList = (List<JSONObject>)((Map<String, Object>)jsonObj.get("map")).get("list");
+		switch (method) {
+		
+			case "GET":
 				
-				for (JSONObject value : jsonList) {
-					Product product = objectMapper.readValue(value.toString(), Product.class);
-					System.out.println(product);
+				if (uri.contains("product")) {
+					
+					if (uri.contains("list")) {
+						List<JSONObject> jsonList = (List<JSONObject>)((Map<String, Object>)jsonObj.get("map")).get("list");
+						
+						System.out.println("\n바인딩 한 내용=");
+						
+						for (JSONObject value : jsonList) {
+							Product product = objectMapper.readValue(value.toString(), Product.class);
+							System.out.println(product);
+						}
+						
+					} else if (uri.contains("get")) {
+						Product product = objectMapper.readValue(jsonObj.toString(), Product.class);
+						System.out.println(product);
+						
+					} else if (uri.contains("update")) {
+						Product product = objectMapper.readValue(jsonObj.toString(), Product.class);
+						System.out.println(product);
+					}
+					
+					
+				} else if (uri.contains("purchase")) {
+					Purchase purchase = objectMapper.readValue(jsonObj.toString(), Purchase.class);
+					System.out.println(purchase);
+					
 				}
 				
-			} else if (uri.contains("get")) {
-				Product product = objectMapper.readValue(jsonObj.toString(), Product.class);
-				System.out.println(product);
+				break;
+	
 				
-			}
-			
-			
-		} else if (uri.contains("purchase")) {
-			Purchase purchase = objectMapper.readValue(jsonObj.toString(), Purchase.class);
-			System.out.println(purchase);
-			
+			case "POST":
+				
+				if (uri.contains("product")) {
+					
+					if (uri.contains("update")) {
+						Product product = objectMapper.readValue(jsonObj.toString(), Product.class);
+						System.out.println(product);
+					}
+					
+					
+				} else if (uri.contains("purchase")) {
+					
+					
+				}
+				
+				break;
+				
 		}
+		
 		
 	}
 
